6c81001a745dc66fe358f4064ac9845a0d5b92ca
RoNeil Boyce <boyceroneil@gmail.com>
2021-01-29 10:34:15 -0500

    work on connecting to database and getting discounted price

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
+import java.util.Random;
+import java.sql.Time;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
+
+public Time getArrivalTime(){
+    final Random random = new Random();
+    final int millisInDay = 24*60*60*1000;
+    Time time = new Time((long)random.nextInt(millisInDay));
+    return time;
+}
+    public double newPrice(int age, double price, String gender){
+        double discounted;
+        if(age<= 12){
+            if(gender.equals("female")){
+            discounted = price-(price*.75);
+            return discounted;
+            }
+            else{
+                discounted= price - (price*.5);
+                return discounted;
+
+            }
+
+        }
+        else if(age >=60){
+            if(gender.equals("female")) {
+                discounted = price - (price * .85);
+                return discounted;
+            }
+                else{
+                    discounted= price-(price*.6);
+                    return discounted;
+                }
+            }
+        else return price;
+
+        }
+
 }
