4cc7eb9ae5ecf0a21621775dd73acd5b0dae6939
Jessica <justinholc18@gmail.com>
2021-01-29 07:30:33 -0800

    user input created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
 
-public class Main {
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+import java.util.Scanner;
 
+public class Main {
+    //Hibernate project by  Justin Holcomb and Roneil Boyce
     public static void main(String[] args) {
-	// write your code here
+        String name,email, phoneNumber, gender,destination;
+        int age;
+        Date date;
+        LocalTime lt;
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		date = new Date(System.currentTimeMillis());
+        Scanner kb = new Scanner(System.in);
+	    System.out.println("welcome to the boarding pass ticket gennerator");
+	    System.out.println("first we will need your name");
+	    name = kb.nextLine();
+	    System.out.println("email?");
+	    email = kb.nextLine();
+	    System.out.println("phone?");
+	    phoneNumber = kb.nextLine();
+	    System.out.println("age?");
+	    age = kb.nextInt();
+	    kb.nextLine();
+	    System.out.println("gender?");
+	    gender = kb.nextLine();
+	    System.out.println("destination?");
+	    destination = kb.nextLine();
+	    while(true) {
+	    	try {
+				System.out.println("finally, what time would you like to depart? (enter the time in the format hh:mm:ss)");
+				String time = kb.nextLine();
+				lt = LocalTime.parse(time);
+				break;
+			}
+	    	catch(DateTimeParseException e)
+			{
+				System.out.println(e.toString());
+				System.out.println("try again!");
+			}
+		}
+	    System.out.println("name: " + name + " email: " + email + " phone: " + phoneNumber + " age: " + age + " gender: " + gender + " destination: " + destination + " departure time: " + lt.toString());
+	    
+
+
+
     }
 }
